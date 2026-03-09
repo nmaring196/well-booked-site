5f4447d35cdd9a439fb320fb83a3bd95998954b0
nmaring196 <nmaring196@gmail.com>
2026-03-09 03:49:03 -0500

    Add Lead and Service interfaces to types.ts

---
+++ types.ts
@@ -0,0 +1,14 @@
+export interface Lead {
+  name: string;
+  email: string;
+  businessName: string;
+  employees: string;
+  address: string;
+  message: string;
+}
+
+export interface Service {
+  title: string;
+  description: string;
+  icon: string;
+}
